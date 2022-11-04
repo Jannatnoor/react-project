ebd73beff8fdfbbb784792cd5b484928cc745ad3
Md Shayemur Rahman <sayemurrahman99@gmail.com>
2022-11-04 19:33:36 +0200

    navbar created

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import Navbar from './component/Navbar';
+
+import './App.css';
+
+const App = () => {
+  //
+  return (
+    <>
+      {/* .jsx */}
+      {/* Component */}
+
+      <Navbar />
+    </>
+  );
+};
+
+export default App;
---
+++ src/component/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav className='navbar'>
+      <h1 className='title'>My Website</h1>
+      <div className='row'>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact Me</li>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
